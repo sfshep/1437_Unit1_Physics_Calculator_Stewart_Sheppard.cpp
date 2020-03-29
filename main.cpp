c3fa3c43699ec56562dc746d0240391b4962f1c2
Fred Sheppard <stewart.sheppard@my.tccd.edu>
2020-03-29 21:18:32 +0000

    Initial Setup

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
+/* Stewart Sheppard
+   COSC 1437-58000
+   Text Editor: Repl.it
+A decision program menu to help students who are taking a Physics
+(Mechanics) class perform simple foundation calculations.*/
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
+int main() 
+{
+
 }
